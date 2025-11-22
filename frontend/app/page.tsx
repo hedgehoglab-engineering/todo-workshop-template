2f9f04cfd7600daee2399157338a3ef712d8af8d
John Hunt <john.hunt@hedgehoglab.com>
2025-11-22 22:39:37 +0000

    Get rid of the rocket icon

--- frontend/app/page.tsx
+++ frontend/app/page.tsx
@@ -1,16 +1,13 @@
 import { Center, Container, Text } from "@mantine/core";
-import { IconRocket } from "@tabler/icons-react";
 import { TodoList } from "./list"; // adjust path if needed
 
 export default function Home() {
   return (
     <Container w='100%' >
       <Center pt={10}>
-        <IconRocket/>
         <Text px={5} size='xl' >
         </Text>
         <TodoList></TodoList>
-        <IconRocket/>
       </Center>
     </Container>
   );
