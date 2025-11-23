7766eeb269d183822c7d05a92057959121a71dfe
John Hunt <john.hunt@hedgehoglab.com>
2025-11-23 12:07:33 +0000

    Add a button for the text input

--- frontend/app/input.tsx
+++ frontend/app/input.tsx
@@ -1,7 +1,10 @@
-import { TextInput } from '@mantine/core';
+import { TextInput , ActionIcon , Group } from '@mantine/core';
+import { IconCircleCheck } from '@tabler/icons-react';
 
 export function TodoInput() {
   return (
+    <>
+    <Group align="flex-end" >
     <TextInput
       size="lg"
       radius="xl"
@@ -9,5 +12,10 @@ export function TodoInput() {
       description="Let's get this done"
       placeholder="Input placeholder"
     />
+    
+    <ActionIcon variant="filled" color="red" size="xl" radius="xl" aria-label="Submit">
+      <IconCircleCheck />
+    </ActionIcon>
+    </Group></>
   );
 }
